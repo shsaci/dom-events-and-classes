2b9ed6f796e46bc436197634f82c42c2ed2cf18b
shsaci <shsaci@yahoo.com.au>
2016-06-17 20:41:04 +1200

    add hide function

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -11,7 +11,9 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue);
+    dots[i].addEventListener('dblclick', hide);
   }
 }
 
@@ -23,7 +25,16 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue');
+  updateCounts();
+}
+
 // CREATE FUNCTION hide HERE
+function hide(evt) {
+  evt.target.classList.toggle('invisible');
+  updateCounts();
+}
 
 function updateCounts () {
   var totals = {
@@ -31,7 +42,7 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
-  
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
   // Once you've done the counting, this function will update the display
